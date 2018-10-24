20984e00426d5cd10f90b6d876ea77c92e2c4b0a
Qi Wang <qiwan@redhat.com>
2018-10-24 12:12:52 -0400

    Add disk usage for ListContainerStats
    (cherry picked from commit 833eb0dd426a6f228c99a82dc3196cfdb317d81d)

--- server/container_stats.go
+++ server/container_stats.go
@@ -1,14 +1,23 @@
 package server
 
 import (
+	"path/filepath"
 	"time"
 
 	"github.com/cri-o/cri-o/internal/oci"
+	crioStorage "github.com/cri-o/cri-o/utils"
+	"github.com/sirupsen/logrus"
 	"golang.org/x/net/context"
 	pb "k8s.io/cri-api/pkg/apis/runtime/v1alpha2"
 )
 
 func buildContainerStats(stats *oci.ContainerStats, container *oci.Container) *pb.ContainerStats {
+	// TODO: Fix this for other storage drivers. This will only work with overlay.
+	diffDir := filepath.Join(filepath.Dir(container.MountPoint()), "diff")
+	bytesUsed, inodeUsed, err := crioStorage.GetDiskUsageStats(diffDir)
+	if err != nil {
+		logrus.Warnf("unable to get disk usage for container %s， %s", container.ID(), err)
+	}
 	return &pb.ContainerStats{
 		Attributes: &pb.ContainerAttributes{
 			Id:          container.ID(),
@@ -24,7 +33,12 @@ func buildContainerStats(stats *oci.ContainerStats, container *oci.Container) *p
 			Timestamp:       stats.SystemNano,
 			WorkingSetBytes: &pb.UInt64Value{Value: stats.MemUsage},
 		},
-		WritableLayer: nil,
+		WritableLayer: &pb.FilesystemUsage{
+			Timestamp:  stats.SystemNano,
+			FsId:       &pb.FilesystemIdentifier{Mountpoint: container.MountPoint()},
+			UsedBytes:  &pb.UInt64Value{Value: bytesUsed},
+			InodesUsed: &pb.UInt64Value{Value: inodeUsed},
+		},
 	}
 }
 
